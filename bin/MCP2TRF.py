547c011153620df8b73c5f75e511094f52650f7a
Diana Lee <nuevecuervos@gmail.com>
2021-11-10 21:31:32 -0800

    Update MCP2TRF.py

--- bin/MCP2TRF.py
+++ bin/MCP2TRF.py
@@ -10,20 +10,21 @@
 
  Requires: 
 
-    phage_functions.ipynb  :  Functions for calculating T based on genome size
+    phageFunctions.py  :  Functions for calculating T based on genome size
     MCP2T_RF_state.db  :  Trained random forest model database
 
 Input: 
-    MCP phage data to analyze. Must include the following columns:
-        'genome_length'
-        'MCP_len'
+    
+    Two options: 
+      1) a fasta format file for up to 2000 MCPs (protein format)
+      2) an MCP phage data to analyze. Must include the following columns:
         'Virus_ID'
         'MCP_Sequence'
+        'MCP_len'
         'IPC'
 
 Output:
-    results\phageResults.csv  :  results of the random forest prediction
-    results\phageResults_db.db  :  database state after the prediction
+    MCP2TResults.csv  :  results of the random forest prediction
 """
 
 # basic imports
@@ -58,6 +59,7 @@ from sklearn import metrics
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqUtils import GC
+from Bio.SeqUtils.IsoelectricPoint import IsoelectricPoint as IP
 
 # phage function imports
 from phageFunctions import tNearest
@@ -107,10 +109,62 @@ dill.load_session('MCP2T_RF_state.db')
 errMar = 0.09
 
 MCP_Type = input("Get in the VAN! Enter MCP input type (1: fasta file, 2: csv file): ")
-
 if MCP_Type=="1":
-    print("Sad Honk. Sorry, I'm currently in the shop.")
+    print("Vroom! Let's go!")
+    MCP_Fasta_Loc = input("Enter file location: ")
+    
+    assert os.path.exists(MCP_Fasta_Loc), "Error: file does not exist at "+str(MCP_Fasta_Loc)
+    MCP_fasta_file = open(MCP_Fasta_Loc,'r+')
+    print("File found. Processing.")
+    # import a single fasta for a whole genome
+    MCP_fasta_data = []
+    for record in SeqIO.parse(MCP_fasta_file, "fasta"):
+        #print(record.description)
+        #print("genome length ",len(record.seq))
+        MCP_fasta_data.append(str(record.description[0:75]))
+        MCP_fasta_data.append(str(record.seq))
+        MCP_fasta_data.append(len(record.seq))
+        MCP_fasta_data.append(IP(str(record.seq)).pi())       
+    MCP_fasta_file.close()
 
+    MCP_fasta_data = np.reshape(np.ravel(MCP_fasta_data), (-1, 4))
+    MCP_fasta_data = pd.DataFrame(MCP_fasta_data)
+    MCP_fasta_data = MCP_fasta_data.rename(columns={0: 'Virus_ID', 1: 'MCP_Sequence', 2: 'MCP_len', 3: 'IPC'})
+    MCP_fasta_data["MCP_len"] = MCP_fasta_data["MCP_len"].astype('int64')
+    MCP_fasta_data["IPC"] = MCP_fasta_data["IPC"].astype('float64')
+    
+    n = len(MCP_fasta_data["Virus_ID"])
+
+    # create the dataset for the random forest
+    freq = createFreq(MCP_fasta_data["MCP_Sequence"], 1)
+    AAT = []
+    for i in range(n):
+        AAT.append(MCP_fasta_data.iloc[i]["Virus_ID"])
+        AAT.append(MCP_fasta_data.iloc[i]["IPC"])
+        AAT.append(MCP_fasta_data.iloc[i]["MCP_len"])
+        for j in range(20):
+            AAT.append(freq[i][j])
+    AAT = np.asarray(np.reshape(np.ravel(AAT), (n, 23)))
+    x_FaaPhage = pd.DataFrame(AAT)
+
+    # assign the features
+    x_FaaActual = (x_FaaPhage.iloc[:,1:23]).astype(float)
+    
+    # predict T-numbers (ignore the error; the trained forest is imported in line 104)
+    y_Pred = rfBest_clf.predict(x_FaaActual)  
+
+    # create an output file
+    y_PredTemp = []
+    
+    for i in range(n):
+        y_PredTemp.append(x_FaaPhage[i][0])
+        y_PredTemp.append(tdict2rev[y_Pred[i]])
+    y_PredTemp = pd.DataFrame(np.reshape(np.ravel(y_PredTemp),(n,2)))
+    y_PredTemp = y_PredTemp.rename(columns={0: 'Virus_ID', 1: 'T_pred'})
+    phageMCP2TResult = MCP_fasta_data.merge(y_PredTemp, how='left', on='Virus_ID')
+    phageMCP2TResult.to_csv(r'MCP2TResults.csv', index=False)
+    print("Success! see MCP2TResults.csv")
+    
 elif MCP_Type=="2":
     print("Vroom! Let's go!")
     print("Your .csv file will require four columns: Virus ID, MCP_Sequence, MCP_len, and IPC")
@@ -151,7 +205,6 @@ elif MCP_Type=="2":
     phageMCP2TResult = MCPData.merge(y_PredTemp, how='left', on='Virus_ID')
     phageMCP2TResult.to_csv(r'MCP2TResults.csv', index=False)
     print("Success! see MCP2TResults.csv")
-  
     
 else:
     print("Sad honk. Invalid input.")
